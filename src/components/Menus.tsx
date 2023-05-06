9d2e4f2c7e898ab775e35cd0012bc9eb7d7c92fe
yusuf1980 <yusufachmad1980@gmail.com>
2023-05-06 16:35:09 +0800

    add products

--- src/components/Menus.tsx
+++ src/components/Menus.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Col, Card, Button } from 'react-bootstrap'
+import { numberWithCommas } from '../utils/util'
 
 const Menus = ({menu}:any) => {
   return (
-    <div>
-        <h2>{menu.nama}</h2>
-    </div>
+    <Col md={4} xs={6} className='mb-4'>
+      <Card className='shadow'>
+        <Card.Img variant="top" src={"assets/images/"+menu.category.nama.toLowerCase()+"/"+menu.gambar} />
+        <Card.Body>
+          <Card.Title>{menu.nama} (<strong>{menu.kode}</strong>)</Card.Title>
+          <Card.Text>
+            Rp. {numberWithCommas(menu.harga)}
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </Col>
   )
 }
 
